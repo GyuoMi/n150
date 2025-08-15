44c435f11499b98e09d5907a7b957acfb2ac9396
GyuoMi <darshnsingh10@gmail.com>
2025-08-16 01:26:01 +0200

    aug 16

--- 547.number_of_provinces.py
+++ 547.number_of_provinces.py
@@ -1,15 +1,16 @@
 class Solution:
-    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
         par = [i for i in range(n)]
         rank = [1] * n
-
+        
         def find(n1):
             res = n1
             while res != par[res]:
                 par[res] = par[par[res]]
                 res = par[res]
             return res
-        
+
         def union(n1, n2):
             p1, p2 = find(n1), find(n2)
             if p1 == p2:
@@ -19,11 +20,14 @@ class Solution:
                 par[p1] = p2
                 rank[p2] += rank[p1]
             else:
-                par[p2] = p1
+                par[p1] = p2
                 rank[p1] += rank[p2]
             return 1
 
         res = n
-        for n1, n2 in edges:
-            res -= union(n1, n2)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if isConnected[i][j] == 1:
+                    res -= union(i, j)
         return res
+
