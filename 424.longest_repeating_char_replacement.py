8103c4dd71862e430c6df124137286c535807aa1
GyuoMi <darshnsingh10@gmail.com>
2025-08-18 03:10:03 +0200

    aug 18

---
+++ 424.longest_repeating_char_replacement.py
@@ -0,0 +1,16 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        res = 0
+        count = {}
+        l = 0
+
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            
+            while (r - l + 1) - max(count.values()) > k:
+                # shift left pointer when the amount of replacements is greater than allowed (k)
+                # before incrementing count of char at left pos
+                count[s[l]] -= 1
+                l += 1
+            res = max(res, r - l + 1)
+        return res
