328575ea4d2b5c36f300635aed9c1d1d01abb2d1
GyuoMi <darshnsingh10@gmail.com>
2025-08-30 23:57:23 +0200

    aug 30

---
+++ 239.sliding_window_maximum.py
@@ -0,0 +1,23 @@
+import collections
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # monotonically decreasing queue
+        output = []
+        #  collections.deque()
+        q = deque() # store indices
+        l = r = 0
+
+        while r < len(nums):
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+
+            q.append(r)
+
+            if l > q[0]:
+                q.popleft()
+
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
+                l += 1
+            r += 1
+        return output
